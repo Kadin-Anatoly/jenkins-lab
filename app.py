cf7c4f2e003a78ba4c934ee2140e2e2447ff9aef
Kadin-Anatoly <anatoly.kadin@mail.ru>
2025-11-14 00:22:12 +0500

    Fix app.py and set correct port 8072

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    student_name = os.getenv('STUDENT_NAME', 'Девопсов Олег')
-    return render_template('index.html', student_name=student_name)
+    student_name = os.getenv('STUDENT_NAME', 'Anatoly Kadin')
+    return f'Hello, {student_name}!'
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 8081))
+    port = int(os.getenv('PORT', 8072))
     app.run(host='0.0.0.0', port=port)
